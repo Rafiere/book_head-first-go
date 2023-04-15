0ea283142d041de05ca0c6bd01e98b63505d45b6
Rafiere <rafapgbr@gmail.com>
2023-04-15 11:20:43 -0300

    feat: add chapter 01 and 02 functions

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github.com/rafiere/head-first-go/chapter01"
+	"github.com/rafiere/head-first-go/chapter02"
+)
+
+func main() {
+
+	/* Chapter 01 */
+
+	chapter01.HelloWorld()
+	chapter01.Variables()
+
+	/* Chapter 02 */
+
+	chapter02.Conditionals()
+}
