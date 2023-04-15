dce8348d67b69ec9cb0b770132cb1ae49f24a63e
Rafiere <rafapgbr@gmail.com>
2023-04-15 11:20:58 -0300

    feat: add chapter 02 code.

---
+++ chapter02/chapter02.go
@@ -0,0 +1,21 @@
+package chapter02
+
+import (
+	"fmt"
+	"time"
+)
+
+func Conditionals() {
+
+	fmt.Println("\n--- CAPÍTULO 02 ---")
+
+	/* Exibindo a data atual: */
+	fmt.Println("\nExibindo a data atual:")
+
+	currentTime := time.Now()
+	fmt.Println(currentTime.Date())
+}
+
+func Loops() {
+
+}
