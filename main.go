7e78207e55c8a1d0da389bf4470f394f0eb8a089
Rafiere <rafapgbr@gmail.com>
2023-04-15 19:44:12 -0300

    feat: add chapter 02, 03 and 04 code

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/rafiere/head-first-go/chapter01"
 	"github.com/rafiere/head-first-go/chapter02"
+	"github.com/rafiere/head-first-go/chapter03"
 )
 
 func main() {
@@ -15,4 +16,12 @@ func main() {
 	/* Chapter 02 */
 
 	chapter02.Conditionals()
+	//chapter02.PassFail()
+	//chapter02.RandomNumberGame()
+
+	/* Chapter 03 */
+	chapter03.FormattingValues()
+	chapter03.SayHi()
+	chapter03.PrintAPositiveNumber(10)
+	chapter03.Pointers()
 }
